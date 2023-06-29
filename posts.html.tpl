c4eba82336529f29c8f0cf1cae1264101ad97ef2
oxou <102489121+oxou@users.noreply.github.com>
2023-06-29 17:50:10 +0200

    Add posts.html.tpl and prepare.php for preparing the posts page which will be used as an alternative page for those who run their browsers with JavaScript disabled.
    Albeit the links on that page will point to raw markdown files
    instead of the traditional Web pages, which may suck for some people
    but you can always use a third-party tool, or use conversion tools
    to generate HTML from Markdown and browse locally.
    
    Links in the markdown files may point to `~/` which is a derivative
    from the Unix/Linux eco-system that points to the HOME directory of
    the logged in user.  I use this same approach to reference the HOME
    directory of a particular post, those resources should be located at
    `/cdn/$id/` so you can do a bit of `sed` magic and replace those
    occurences if you want.
    
    The reason I did it that way is to preserve bandwidth, as writing
    the entire link is not very bandwidth friendly, but also the page
    domain may change in the future and that has to be reflected
    dynamically, which is at the moment done through JavaScript.
    
    Anyways, bye.

---
+++ posts.html.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>oxou - posts</title>
+</head>
+<body>
+    <h1>oxou - posts</h1>
+    <p>These entries point to plain-text markdown files, don't expect them to show up like a real HTML page.<br>For that you need JavaScript or a Desktop app like <a href="https://obsidian.md/" target="_blank">Obsidian</a> to view.</p>
+    <div>{{posts}}</div>
+</body>
+</html>
---
+++ prepare.php
@@ -0,0 +1,86 @@
+<?php
+
+// Copyright (C) 2023 Nurudin Imsirovic <github.com/oxou>
+//
+// Prepare the list of posts from ./data/posts.json to be
+// referenced in ./posts.html for those browsing the blog
+// without JavaScript enabled.  Don't be crazy and enable
+// JavaScript.  Thanks.
+//
+// Created: 2023-06-29 05:39 PM
+// Updated: 2023-06-29 05:49 PM
+
+// Return codes
+// 1 = json_decode not found
+// 2 = ./data/posts.json not found
+// 4 = ./posts.html.tpl not found
+
+$html_buffer = "";
+
+function _error($a) {
+    printf("\033[31m[ERROR]\033[0m $a\n");
+}
+
+if (!function_exists("json_decode")) {
+    _error("json_decode() does not exist. Can't decode JSON.");
+    exit(1);
+}
+
+function _html_write_entry($a, $b, $c) {
+    return sprintf('<a href="/data/post/'.$a.'/index.md"><b>'.$b.'</b> '.$c.'</a><br>');
+}
+
+// JSON must exist
+if (!file_exists("./data/posts.json")) {
+    _error("./data/posts.json does not exist. Cannot retrieve list of posts.");
+    exit(2);
+}
+
+// Posts template must exist
+if (!file_exists("./posts.html.tpl")) {
+    _error("./posts.html.tpl does not exist. Cannot build posts.html.");
+    exit(4);
+}
+
+$template = file_get_contents("./posts.html.tpl");
+$data = file_get_contents("./data/posts.json");
+$data = json_decode($data, JSON_OBJECT_AS_ARRAY);
+
+foreach ($data as $json_obj) {
+    $html_buffer .= _html_write_entry(
+        $json_obj["id"],
+        $json_obj["date"],
+        $json_obj["title"]
+    );
+}
+
+// Write posts.html
+$new_data = str_replace("{{posts}}", $html_buffer, $template);
+
+// Convert tabs to spaces
+$new_data = str_replace("\t", ' ', $new_data);
+
+// Convert CRLF to LF
+$new_data = str_replace(
+    array("\r\n", "\r"),
+    array("\n", ''),
+    $new_data
+);
+
+// Get rid of tabs
+while (strpos($new_data, "\n ") !== false) {
+    $new_data = str_replace("\n ", "\n", $new_data);
+}
+
+// Finally destroy all newlines
+$new_data = str_replace(
+    array("\r\n", "\r", "\n"),
+    array(''),
+    $new_data
+);
+
+// Write file
+file_put_contents("./posts.html", $new_data);
+echo "OK\n";
+
+?>
